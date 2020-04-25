95246c9ad5d9a8ea8d0fec70928bfe8449b6fbf9
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 02:55:47 -0700

    obreez

--- sketch/main.js
+++ sketch/main.js
@@ -1,8 +1,28 @@
+let circles = [];
+
 function setup() {
   createCanvas(600, 800);
 }
 
+function rand_color() {
+  const randomColor = color(
+    parseInt(Math.random() * 255),
+    parseInt(Math.random() * 255),
+    parseInt(Math.random() * 255)
+  );
+  return randomColor;
+}
+
+function mousePressed() {
+  circles.push([mouseX, mouseY, rand_color()]);
+}
+
 function draw() {
   background(255);
-  circle(mouseX, mouseY, 20);
+  for (let i = 0; i < circles.length; i++) {
+    circle(circles[i][0], circles[i][1], 20);
+    fill(circles[i][2]);
+  }
+  fill(0);
+  noStroke();
 }
