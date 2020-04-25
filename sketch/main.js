9e8840cc74d915b9ed1ebc9c2afc7e2ddf7d259f
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 01:09:09 -0700

    circle color

--- sketch/main.js
+++ sketch/main.js
@@ -1,7 +1,15 @@
+let circleSize;
+
 function setup() {
+  circleSize = createSlider(0, 255, 0);
+  circleSize.position(10, 10);
+  circleSize.style("width", 500 + "px");
   createCanvas(600, 800);
 }
 
 function draw() {
-  circle(50, 50, 20);
+  let val = circleSize.value();
+  noStroke();
+  circle(300, 300, 300);
+  fill(val);
 }
