53cf04fbb6c0745f781197f1deb8f6e1d1411e92
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 03:31:41 -0700

    bounce

--- sketch/main.js
+++ sketch/main.js
@@ -1,7 +1,27 @@
+let x = 0;
+let y = 0;
+let speedX = 10;
+let speedY = 10;
+
 function setup() {
-  createCanvas(600, 800);
+  createCanvas(600, 500);
+  y = height / 2;
+  frameRate(60);
 }
 
 function draw() {
-  circle(50, 50, 20);
+  background(255);
+
+  // mover
+  circle(x, y, 50);
+  fill(125);
+  if (x >= width || x < 0) {
+    speedX *= -1;
+  }
+  if (y >= height || y < 0) {
+    speedY *= -1;
+  }
+
+  x += speedX;
+  y += speedY;
 }
