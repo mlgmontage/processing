f4dca4e48598213d20629078956ba89fff608ba4
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 10:58:45 -0700

    flyweight pattern

--- sketch/main.js
+++ sketch/main.js
@@ -1,8 +1,46 @@
+class IceCream {
+  constructor(flavour, price) {
+    this._flavour = flavour;
+    this._price = price;
+  }
+}
+
+class IceCreamFactory {
+  constructor() {
+    this._icecreams = [];
+  }
+
+  createIceCream(flavour, price) {
+    let icecream = this.getIceCream(flavour);
+    if (icecream) {
+      return icecream;
+    } else {
+      let newIceCream = new IceCream(flavour, price);
+      this._icecreams.push(newIceCream);
+      return newIceCream;
+    }
+  }
+
+  getIceCream(flavour) {
+    return this._icecreams.find((icecream) => icecream._flavour === flavour);
+  }
+}
+
 function setup() {
   createCanvas(600, 800);
   createP("Object oriented programming")
     .position(50, 10)
     .style("font-size", "48px");
+
+  // Creating factory
+  const factory = new IceCreamFactory();
+
+  // Creating product
+  const choco = factory.createIceCream("chocolate and vanilla", 15);
+  const vanilla = factory.createIceCream("chocolate and vanilla", 15);
+
+  print(factory._icecreams); // array contains only one element
+  print(choco === vanilla); // they are same
 }
 
 function draw() {
