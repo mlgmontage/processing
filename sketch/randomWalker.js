b4b7e98fd5d2eaa3f6dff8c8181dbdf7aaf99be1
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 03:59:32 -0700

    tremor

--- sketch/randomWalker.js
+++ sketch/randomWalker.js
@@ -1,19 +1,20 @@
 let RandomWalker = {
   x: 400,
   y: 300,
-  circleSize: 10,
+  stepSize: 3,
+  circleSize: 40,
 
   step: function () {
     const direction = rand(1, 4);
 
     if (direction === 1) {
-      this.x += this.circleSize;
+      this.x += this.stepSize;
     } else if (direction === 2) {
-      this.x -= this.circleSize;
+      this.x -= this.stepSize;
     } else if (direction === 3) {
-      this.y += this.circleSize;
+      this.y += this.stepSize;
     } else {
-      this.y -= this.circleSize;
+      this.y -= this.stepSize;
     }
   },
 
