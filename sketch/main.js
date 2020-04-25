ad923d979a4c2b4d46cb4139e57539bbf9d343f9
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 06:33:23 -0700

    draw lines

--- sketch/main.js
+++ sketch/main.js
@@ -1,9 +1,32 @@
+let isMousePressed = false;
+let lines = [];
+let Line = [];
+
 function setup() {
   createCanvas(600, 800);
 }
 
-function mouseClicked() {}
+function mousePressed() {
+  isMousePressed = true;
+  Line.push(mouseX);
+  Line.push(mouseY);
+}
+
+function mouseReleased() {
+  Line.push(mouseX);
+  Line.push(mouseY);
+  isMousePressed = false;
+  lines.push(Line);
+  Line = [];
+}
 
 function draw() {
-  circle(width / 2, height / 2, 20);
+  background(255);
+  if (isMousePressed) {
+    line(Line[0], Line[1], mouseX, mouseY);
+  }
+
+  for (let i = 0; i < lines.length; i++) {
+    line(lines[i][0], lines[i][1], lines[i][2], lines[i][3]);
+  }
 }
