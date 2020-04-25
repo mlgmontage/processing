1a1f1fea27ac4c3c145bc3a8b84e192ed447bb4a
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 01:16:13 -0700

    simple color picker

--- sketch/main.js
+++ sketch/main.js
@@ -1,15 +1,32 @@
-let circleSize;
+let red, green, blue;
 
 function setup() {
-  circleSize = createSlider(0, 255, 0);
-  circleSize.position(10, 10);
-  circleSize.style("width", 500 + "px");
+  red = createSlider(0, 255, 0);
+  red.position(10, 10);
+  red.style("width", 500 + "px");
+
+  green = createSlider(0, 255, 0);
+  green.position(10, 30);
+  green.style("width", 500 + "px");
+
+  blue = createSlider(0, 255, 0);
+  blue.position(10, 50);
+  blue.style("width", 500 + "px");
+
   createCanvas(600, 800);
 }
 
 function draw() {
-  let val = circleSize.value();
-  noStroke();
+  // color values
+  let red_val = red.value();
+  let green_val = green.value();
+  let blue_val = blue.value();
+
+  noStroke(); // without outline
+  // draw circle
   circle(300, 300, 300);
-  fill(val);
+  // color
+
+  let circleColor = color(red_val, green_val, blue_val);
+  fill(circleColor);
 }
