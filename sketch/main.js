4b869ce65e9ada8b04685edfdab09d522757b4ce
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 07:09:59 -0700

    rectangle

--- sketch/main.js
+++ sketch/main.js
@@ -13,8 +13,8 @@ function mousePressed() {
 }
 
 function mouseReleased() {
-  Line.push(mouseX);
-  Line.push(mouseY);
+  Line.push(mouseX - Line[0]);
+  Line.push(mouseY - Line[1]);
   isMousePressed = false;
   shapes.push(Line);
   Line = [];
@@ -23,10 +23,10 @@ function mouseReleased() {
 function draw() {
   background(255);
   if (isMousePressed) {
-    line(Line[0], Line[1], mouseX, mouseY);
+    rect(Line[0], Line[1], mouseX - Line[0], mouseY - Line[1]);
   }
 
   for (let i = 0; i < shapes.length; i++) {
-    line(shapes[i][0], shapes[i][1], shapes[i][2], shapes[i][3]);
+    rect(shapes[i][0], shapes[i][1], shapes[i][2], shapes[i][3]);
   }
 }
