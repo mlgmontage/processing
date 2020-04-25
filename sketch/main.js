825ac255609bc3ff11a18e24ba7be58c0a5c2154
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 02:30:06 -0700

    hooray

--- sketch/main.js
+++ sketch/main.js
@@ -1,7 +1,20 @@
+// store the previous position
+let prev_position = [];
+
 function setup() {
   createCanvas(600, 800);
 }
 
+function mouseClicked() {
+  if (prev_position.length == 0) {
+    line(width / 2, height / 2, mouseX, mouseY);
+  } else {
+    line(prev_position[0], prev_position[1], mouseX, mouseY);
+  }
+  prev_position[0] = mouseX;
+  prev_position[1] = mouseY;
+}
+
 function draw() {
-  circle(50, 50, 20);
+  circle(width / 2, height / 2, 20);
 }
