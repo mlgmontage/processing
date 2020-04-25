ec12ecdd5cf00c8f707bd8a2f5806dd90c15b06a
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 09:15:53 -0700

    constructor pattern lol

--- sketch/main.js
+++ sketch/main.js
@@ -1,8 +1,28 @@
+class Hero {
+  constructor(name, ability) {
+    this.name = name;
+    this.ability = ability;
+  }
+
+  getDescription() {
+    return `${this.name} can ${this.ability}`;
+  }
+}
+
+let IronMan = new Hero("Ironman", "fly");
+let SuperMan = new Hero("Superman", "fly");
+const Batman = new Hero("Batman", "drive");
+
 function setup() {
   createCanvas(600, 800);
   createP("Object oriented programming")
     .position(50, 10)
     .style("font-size", "48px");
+
+  // Objects
+  print(IronMan.getDescription());
+  print(SuperMan.getDescription());
+  print(Batman.getDescription());
 }
 
 function draw() {
