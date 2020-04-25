5a83abac8f28a48299ec378d6d3214545fadb5bd
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 00:43:39 -0700

    tangent function

--- sketch/main.js
+++ sketch/main.js
@@ -2,6 +2,11 @@ function setup() {
   createCanvas(600, 800);
 }
 
+const circleSize = 1;
+
 function draw() {
-  circle(50, 50, 20);
+  // tangent function
+  for (let i = 0; i < width; i += circleSize) {
+    circle(i, Math.tan(i) * 10 + 300, circleSize);
+  }
 }
