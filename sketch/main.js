60009ff1ff036580334f1f0d9958a6bdae07f606
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 02:43:00 -0700

    background(255)

--- sketch/main.js
+++ sketch/main.js
@@ -1,8 +1,10 @@
 function setup() {
   createCanvas(600, 800);
+  frameRate(10);
 }
 
 function draw() {
+  background(255);
   for (let i = 0; i < 20; i++) {
     noStroke();
     fill(randColor());
