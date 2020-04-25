5483c42964de58306579496c8dbe358fd079d7c0
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 10:30:52 -0700

    simple factory

--- sketch/main.js
+++ sketch/main.js
@@ -1,8 +1,65 @@
+class Basketball {
+  constructor() {
+    this._type = "Basketball";
+  }
+  bounce() {
+    return "You bounced basketball ball";
+  }
+}
+
+class Football {
+  constructor() {
+    this._type = "Football";
+  }
+  kick() {
+    return "You kicked football ball";
+  }
+}
+
+class Bouling {
+  constructor() {
+    this._type = "Bouling";
+  }
+}
+
+class BallFactory {
+  constructor() {
+    this.create = function (type) {
+      let ball;
+      if (type === "football" || type === "soccer") ball = new Football();
+
+      if (type === "basketball") ball = new Basketball();
+      if (type === "bouling") ball = new Bouling();
+
+      ball.roll = function () {
+        return `You rolled ${this._type} ball`;
+      };
+
+      return ball;
+    };
+  }
+}
+
 function setup() {
   createCanvas(600, 800);
   createP("Object oriented programming")
     .position(50, 10)
     .style("font-size", "48px");
+
+  // Factory
+  const factory = new BallFactory();
+
+  // Product
+  const basketball = factory.create("basketball");
+  print(basketball.bounce());
+  print(basketball.roll());
+
+  const soccer = factory.create("soccer");
+  print(soccer.roll());
+  print(soccer.kick());
+
+  const bouling = factory.create("bouling");
+  print(bouling.roll());
 }
 
 function draw() {
