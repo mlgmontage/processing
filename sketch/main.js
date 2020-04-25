b5486d3459cc9ae165a629b7b50c1d328ca85f8b
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 00:08:36 -0700

    rect bubbles

--- sketch/main.js
+++ sketch/main.js
@@ -6,6 +6,6 @@ function draw() {
   for (let i = 0; i < 20; i++) {
     noStroke();
     fill(randColor());
-    circle(rand(10, 800), rand(10, 800), rand(10, 50));
+    rect(rand(10, 800), rand(10, 800), rand(10, 100), rand(10, 100));
   }
 }
