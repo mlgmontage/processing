82f7e29a3eb35247c30e1859a85fcb8c2fd54482
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 00:05:29 -0700

    colorfull bubbles

--- sketch/main.js
+++ sketch/main.js
@@ -4,6 +4,8 @@ function setup() {
 
 function draw() {
   for (let i = 0; i < 20; i++) {
+    noStroke();
+    fill(randColor());
     circle(rand(10, 800), rand(10, 800), rand(10, 50));
   }
 }
--- sketch/utils.js
+++ sketch/utils.js
@@ -2,3 +2,7 @@ function rand(start, finish) {
   const randomNumber = parseInt(Math.random() * finish);
   return randomNumber + start;
 }
+
+function randColor() {
+  return color(rand(0, 250), rand(0, 250), rand(0, 250));
+}
