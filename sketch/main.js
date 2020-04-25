91dbb4744f7794a1c2dfa795c362fb7a88f9a1d8
mlgmontage <kettleofrassel1@gmail.com>
2020-04-25 03:41:46 -0700

    colorfull bounce

--- sketch/main.js
+++ sketch/main.js
@@ -1,12 +1,24 @@
 let x = 0;
 let y = 0;
-let speedX = 10;
-let speedY = 10;
+let speedX = 5;
+let speedY = 8;
+let bounceColor;
 
 function setup() {
   createCanvas(600, 500);
+  bounceColor = randColor();
   y = height / 2;
   frameRate(60);
+}
+
+function randColor() {
+  const randomColor = color(
+    parseInt(Math.random() * 255),
+    parseInt(Math.random() * 255),
+    parseInt(Math.random() * 255)
+  );
+
+  return randomColor;
 }
 
 function draw() {
@@ -14,12 +26,14 @@ function draw() {
 
   // mover
   circle(x, y, 50);
-  fill(125);
+  fill(bounceColor);
   if (x >= width || x < 0) {
     speedX *= -1;
+    bounceColor = randColor();
   }
   if (y >= height || y < 0) {
     speedY *= -1;
+    bounceColor = randColor();
   }
 
   x += speedX;
